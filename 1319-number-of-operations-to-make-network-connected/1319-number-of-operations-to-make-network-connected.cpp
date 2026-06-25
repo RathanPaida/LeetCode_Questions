b135e768f637405fd015fa18735a4db27541068d
RathanPaida <paidarajarathan@gmail.com>
2026-06-25 19:23:08 +0530

    Time: 0 ms (100%), Space: 42.2 MB (74.94%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,50 @@
-class Solution {
+class DSU {
 public:
-    void dfs(int i,vector<bool> &vis,vector<vector<int>> &edges){
-        for(int j:edges[i]){
-            if(!vis[j]){
-                vis[j]=true;
-                dfs(j,vis,edges);
-            }
+    vector<int> parent, size;
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if (pu == pv) return;
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
         }
     }
+};
+class Solution {
+public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1){
+        if (connections.size() < n - 1)
             return -1;
+        DSU ds(n);
+        int extra = 0;
+        for (auto &edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+            if (ds.findUPar(u) == ds.findUPar(v))
+                extra++;
+            else
+                ds.unionBySize(u, v);
         }
-        vector<bool> vis(n,false);
-        int ans=0;
-        vector<vector<int>> edges(n);
-        for(auto i:connections){
-            edges[i[0]].push_back(i[1]);
-            edges[i[1]].push_back(i[0]);
+        int components = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.findUPar(i) == i)
+                components++;
         }
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                vis[i]=true;
-                dfs(i,vis,edges);
-                ans++;
-            }
-        }
-        return ans-1;
+        return (extra >= components - 1) ? components - 1 : -1;
     }
 };
