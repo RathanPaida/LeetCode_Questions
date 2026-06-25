90df9b370628ca1a113dac313a14f9045c076220
RathanPaida <paidarajarathan@gmail.com>
2026-06-25 20:49:30 +0530

    Time: 34 ms (40.56%), Space: 18.3 MB (90.45%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,25 +1,46 @@
+class Disjoint{
+    public:
+        vector<int> parent , size;
+        Disjoint(int n){
+            parent.resize(n , 0);
+            size.resize(n , 1);
+            for(int i=0;i<n;i++){
+                parent[i] = i;
+            }
+        }
+        int findUPar(int node){
+            if(node == parent[node]) return node;
+            return parent[node] = findUPar(parent[node]);
+        }
+        void dsunion(int u , int v){
+            if(findUPar(u) == findUPar(v)) return;
+            int ulpu = findUPar(u);
+            int ulpv = findUPar(v);
+            if(size[ulpu] < size[ulpv]){
+                size[ulpv] += size[ulpu];
+                parent[ulpu] = ulpv;
+            } else{
+                size[ulpu] += size[ulpv];
+                parent[ulpv] = ulpu;
+            }
+            return;
+        }
+};
+
 class Solution {
 public:
-    void dfs(int node, vector<bool>& visited, vector<vector<int>>& stones) {
-        visited[node] = 1;
-        for (int j = 0; j < stones.size(); j++) {
-            if (!(visited[j]) &&
-                (stones[j][0] == stones[node][0] || stones[j][1] == stones[node][1])) {
-                dfs(j, visited, stones);
-            }
-        }
-    }
-
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        vector<bool> visited(n, 0);
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                count++;
-                dfs(i, visited, stones);
+        Disjoint st(n);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0] == stones[j][0] || stones[j][1] == stones[i][1]) st.dsunion(i , j);
             }
         }
-        return n - count;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(st.parent[i] == i) ans += st.size[i]-1; 
+        }
+        return ans;
     }
 };
