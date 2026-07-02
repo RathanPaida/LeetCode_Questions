c0027f2ba2bb05c93736cf4d373e34c3c6c8f4fb
RathanPaida <paidarajarathan@gmail.com>
2026-07-03 01:57:40 +0530

    Time: 33 ms (89.77%), Space: 21.7 MB (50.25%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,33 +1,21 @@
 class Solution {
-private:
-    bool isPredecessor(const string& word1, const string& word2) {
-        if (word1.length() + 1 != word2.length()) return false;
-        int i = 0;
-        for (char c : word2) {
-            if (i < word1.length() && word1[i] == c) {
-                i++;
-            }
-        }
-        return i == word1.length();
-    }
-
 public:
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), [](string& a,string& b) {
             return a.length() < b.length();
         });
-        int n = words.size();
-        vector<int> dp(n, 1);
+        unordered_map<string, int> dp;
         int max_chain = 1;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (isPredecessor(words[j], words[i])) {
-                    dp[i] = max(dp[i], dp[j] + 1);
+        for (const string& word : words) {
+            dp[word] = 1;
+            for (int i = 0; i < word.length(); ++i) {
+                string prev_word = word.substr(0, i) + word.substr(i + 1);                
+                if (dp.find(prev_word) != dp.end()) {
+                    dp[word] = max(dp[word], dp[prev_word] + 1);
                 }
             }
-            max_chain = max(max_chain, dp[i]);
+            max_chain = max(max_chain, dp[word]);
         }
-        
         return max_chain;
     }
 };
