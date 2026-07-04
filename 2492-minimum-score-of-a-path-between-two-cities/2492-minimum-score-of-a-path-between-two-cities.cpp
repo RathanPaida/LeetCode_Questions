ad6d4931072f3322f85df65800a67b22aacfff2c
RathanPaida <paidarajarathan@gmail.com>
2026-07-04 14:40:04 +0530

    Time: 19 ms (90.5%), Space: 108.5 MB (92.66%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void solve( int u,int &mini,vector<bool> &vis,vector<vector<pair<int,int>>> &ed){
-        if(vis[u-1]){
-            return;
-        }
-        vis[u-1]=true;
-        for(auto i:ed[u]){
-            mini=min(mini,i.second);
-            solve(i.first,mini,vis,ed);
-        }
-
-    }
     int minScore(int n, vector<vector<int>>& roads) {
-        int mini=INT_MAX;
-        vector<vector<pair<int,int>>> ed(n+1);
-        for(auto i:roads){
-            ed[i[0]].push_back({i[1],i[2]});
-            ed[i[1]].push_back({i[0],i[2]});
+        vector<int> parent(n + 1);
+        vector<int> min_edge(n + 1, INT_MAX);
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
         }
-        vector<bool> vis(n,false);
-        solve(1,mini,vis,ed);
-        return mini;
+        function<int(int)> find = [&](int x) {
+            if (parent[x] == x) return x;
+            return parent[x] = find(parent[x]);
+        };
+        for (const auto& road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int distance = road[2];
+            int rootU = find(u);
+            int rootV = find(v);
+            if (rootU != rootV) {
+                parent[rootV] = rootU;
+                min_edge[rootU] = min({min_edge[rootU], min_edge[rootV], distance});
+            } else {
+                min_edge[rootU] = min(min_edge[rootU], distance);
+            }
+        }
+        return min_edge[find(1)];
     }
 };
