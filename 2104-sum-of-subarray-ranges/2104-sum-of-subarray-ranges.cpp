90bfb4c3e7c4287750231db2ab158c0b00632369
RathanPaida <paidarajarathan@gmail.com>
2026-07-04 23:39:40 +0530

    Time: 3 ms (73.68%), Space: 15.5 MB (69.5%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        stack<int> stk; 
+        stack<int> s;
+        for(int i = n-1; i >= 0; i--){
+            while(!stk.empty() && nums[stk.top()] <= nums[i]) stk.pop();
+            right[i] = stk.empty() ? n-i : stk.top() - i;
+            stk.push(i);
+        }
+        for(int i = 0; i < n;i++){
+            while(!s.empty() && nums[s.top()] < nums[i]) s.pop();
+            left[i] = s.empty() ? i+1 : i - s.top();
+            s.push(i); 
+        }
+        long long largest = 0;
+        for(int i = 0; i <n;i++){
+            largest += (1LL * left[i] * right[i] * nums[i]);
+        }
+        stk = stack<int>();
+        s = stack<int>();
+        for(int i = n-1; i >= 0; i--){
+            while(!stk.empty() && nums[stk.top()] >= nums[i]) stk.pop();
+            right[i] = stk.empty() ? n-i : stk.top() - i;
+            stk.push(i);
+        }
+        for(int i = 0; i < n;i++){
+            while(!s.empty() && nums[s.top()] > nums[i]) s.pop();
+            left[i] = s.empty() ? i+1 : i - s.top();
+            s.push(i); 
+        }
+        long long smallest = 0;
+        for(int i = 0; i <n;i++){
+            smallest += (1LL * left[i] * right[i] * nums[i]);
+        }
+        return largest - smallest;
+    }
+};
