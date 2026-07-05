7c09205d1107f584ebbae11539dcb15724f283f4
RathanPaida <paidarajarathan@gmail.com>
2026-07-05 10:11:58 +0530

    Time: 6 ms (82.23%), Space: 11.8 MB (51.22%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -9,47 +9,51 @@ public:
         ways[n - 1][n - 1] = 1;
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (board[i][j] == 'X') {
+                if(board[i][j]=='X'){
                     continue;
                 }
-                if (i == n - 1 && j == n - 1) {
+                if(i==n-1&&j==n-1){
                     continue;
                 }
-                int bestScore = -1;
-                long long totalWays = 0;
-                if (i + 1 < n && maxScore[i + 1][j] != -1) {
-                    if (maxScore[i + 1][j] > bestScore) {
-                        bestScore = maxScore[i + 1][j];
-                        totalWays = ways[i + 1][j];
-                    } else if (maxScore[i + 1][j] == bestScore) {
-                        totalWays = (totalWays + ways[i + 1][j]) % mod;
+                int bs=-1;
+                int tw=0;
+                if(i+1<n&&maxScore[i+1][j]!=-1){
+                    if(maxScore[i+1][j]>bs){
+                        bs=maxScore[i+1][j];
+                        tw=ways[i+1][j];
+                    }
+                    else if(maxScore[i+1][j]==bs){
+                        tw=(tw+ways[i+1][j])%mod;
                     }
                 }
-                if (j + 1 < n && maxScore[i][j + 1] != -1) {
-                    if (maxScore[i][j + 1] > bestScore) {
-                        bestScore = maxScore[i][j + 1];
-                        totalWays = ways[i][j + 1];
-                    } else if (maxScore[i][j + 1] == bestScore) {
-                        totalWays = (totalWays + ways[i][j + 1]) % mod;
+                if(j+1<n&&maxScore[i][j+1]!=-1){
+                    if(maxScore[i][j+1]>bs){
+                        bs=maxScore[i][j+1];
+                        tw=ways[i][j+1];
+                    }
+                    else if(maxScore[i][j+1]==bs){
+                        tw=(tw+ways[i][j+1])%mod;
                     }
                 }
-                if (i + 1 < n && j + 1 < n && maxScore[i + 1][j + 1] != -1) {
-                    if (maxScore[i + 1][j + 1] > bestScore) {
-                        bestScore = maxScore[i + 1][j + 1];
-                        totalWays = ways[i + 1][j + 1];
-                    } else if (maxScore[i + 1][j + 1] == bestScore) {
-                        totalWays = (totalWays + ways[i + 1][j + 1]) % mod;
+                if(i+1<n&&j+1<n&&maxScore[i+1][j+1]!=-1){
+                    if(maxScore[i+1][j+1]>bs){
+                        bs=maxScore[i+1][j+1];
+                        tw=(ways[i+1][j+1])%mod;
+                    }
+                    else if(maxScore[i+1][j+1]==bs){
+                        tw=(tw+ways[i+1][j+1])%mod;
                     }
                 }
-                if (bestScore == -1) {
+                if(bs==-1){
                     continue;
                 }
-                int curr = 0;
-                if (board[i][j] >= '1' && board[i][j] <= '9') {
-                    curr = board[i][j] - '0';
+                if(board[i][j]>='1'&&board[i][j]<='9'){
+                    maxScore[i][j]=bs+(board[i][j]-'0');
                 }
-                maxScore[i][j] = bestScore + curr;
-                ways[i][j] = totalWays % mod;
+                else{
+                    maxScore[i][j]=bs;
+                }
+                ways[i][j]=tw;
             }
         }
         if (maxScore[0][0] == -1) {
