2fec4c8a2d2f0f22ad04fe39e9f1d6bb5f86422b
RathanPaida <paidarajarathan@gmail.com>
2026-07-09 15:09:52 +0530

    Time: 55 ms (30.76%), Space: 54.7 MB (12.16%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -1,9 +1,4 @@
-/**
- * @param {Function} fn
- * @param {number} t
- * @return {Function}
- */
-var timeLimit = function(fn, t) {
+ var timeLimit = function(fn, t) {
     
     return async function(...args) {
         const timeoutPromise = new Promise((resolve, reject) => {
@@ -17,8 +12,3 @@ var timeLimit = function(fn, t) {
         ]);
     }
 };
-
-/**
- * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
- * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
- */
