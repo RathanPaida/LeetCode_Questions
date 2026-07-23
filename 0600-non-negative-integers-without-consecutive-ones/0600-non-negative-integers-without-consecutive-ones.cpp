b19947e46f710de2a0c33b7d7cdb99fdb202d727
RathanPaida <paidarajarathan@gmail.com>
2026-07-24 00:33:59 +0530

    Time: 2 ms (39.79%), Space: 8.4 MB (64.19%) - LeetHub

---
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findIntegers(int n) {
+        vector<int> f(32, 0);
+        f[0] = 1;
+        f[1] = 2;
+        for (int i = 2; i < 32; ++i) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        int ans = 0;
+        int prev_bit = 0;
+        for (int i = 31; i >= 0; --i) {
+            if ((n & (1 << i)) != 0) {
+                ans += f[i];
+                if (prev_bit == 1) {
+                    return ans;
+                }
+                prev_bit = 1;
+            } else {
+                prev_bit = 0;
+            }
+        }
+        return ans + 1;
+    }
+};
